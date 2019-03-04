3be428bb33360eaee94b2a6e4d6e922c16208452
Drew Winstel <drewbrew@users.noreply.github.com>
2019-03-04 12:07:35 -0600

    Add basic beer and manufacturer models

---
+++ lib/beer.dart
@@ -0,0 +1,53 @@
+import 'package:decimal/decimal.dart';
+import 'manufacturer.dart';
+import 'color.dart';
+
+class Beer {
+  final Manufacturer manufacturer;
+  final String name;
+  final String style;
+  final String styleCategory;
+  final bool inProduction;
+  final Decimal abv;
+  final Decimal ibu;
+  final HideableHexColor color;
+  final String untappdURL;
+  final String beerAdvocateURL;
+  final String rateBeerURL;
+  final String logoURL;
+  final String manufacturerURL;
+  final String taphunterURL;
+
+  Beer(
+      this.manufacturer,
+      this.name,
+      this.style,
+      this.styleCategory,
+      this.inProduction,
+      this.abv,
+      this.ibu,
+      this.color,
+      this.untappdURL,
+      this.beerAdvocateURL,
+      this.rateBeerURL,
+      this.logoURL,
+      this.manufacturerURL,
+      this.taphunterURL,
+  );
+
+  Beer.fromJson(Map<String, dynamic> json):
+    name = json['name'],
+    manufacturer = Manufacturer.fromJson(json['manufacturer']),
+    beerAdvocateURL = json['beer_advocate_url'],
+    manufacturerURL = json['manufacturer_url'],
+    logoURL = json['logo_url'],
+    untappdURL = json['untappd_url'],
+    taphunterURL = json['taphunter_url'],
+    abv = Decimal.parse(json['abv']),
+    ibu = Decimal.parse(json['ibu']),
+    inProduction = json['in_production'],
+    color = HideableHexColor(json['color_html']),
+    rateBeerURL = json['rate_beer_url'],
+    style = json['style']['name'],
+    styleCategory = json['style']['category']['name'];
+}
---
+++ lib/manufacturer.dart
@@ -0,0 +1,34 @@
+class Manufacturer {
+  final String name;
+  final String url;
+  final String location;
+  final String logoURL;
+  final String facebookURL;
+  final String twitterHandle;
+  final String instagramHandle;
+  final String untappdURL;
+  final String taphunterURL;
+
+  Manufacturer(
+      this.name,
+      this.url,
+      this.location,
+      this.logoURL,
+      this.facebookURL,
+      this.twitterHandle,
+      this.instagramHandle,
+      this.untappdURL,
+      this.taphunterURL,
+  );
+
+  Manufacturer.fromJson(Map<String, dynamic> json):
+      name = json['name'],
+      url = json['url'],
+      location = json['location'],
+      logoURL = json['logo_url'],
+      facebookURL = json['facebook_url'],
+      twitterHandle = json['twitter_handle'],
+      instagramHandle = json['instagram_handle'],
+      untappdURL = json['untappd_url'],
+      taphunterURL = json['taphunter_url'];
+}
