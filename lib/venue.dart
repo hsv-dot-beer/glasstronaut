f848d7e59d4894fd3737af3c3ab463ac1d3e5442
Drew Winstel <drewbrew@users.noreply.github.com>
2019-06-25 20:42:04 -0500

    Improve venue model
    Use auto-generated code for completeness

--- lib/venue.dart
+++ lib/venue.dart
@@ -1,49 +1,64 @@
-import 'package:decimal/decimal.dart';
-import 'manufacturer.dart';
-import 'color.dart';
 
 class Venue {
-  final int id;
-  final String name;
-  final String timeZone;
-  final String address;
-  final String city;
-  final String state;
-  final String postalCode;
-  final String url;
-  final String facebookURL;
-  final String twitterHandle;
-  final String instagramHandle;
-  final String untappdURL;
+  int id;
+  String timeZone;
+  String tapListProviderDisplay;
+  String name;
+  String address;
+  String city;
+  String state;
+  String postalCode;
+  String country;
+  String website;
+  String facebookPage;
+  String twitterHandle;
+  String instagramHandle;
+  String tapListProvider;
+  String untappdUrl;
+  String email;
+  String phoneNumber;
+  String logoUrl;
+  String slug;
 
-  Venue(
-    this.id,
+  Venue({this.id,
+    this.timeZone,
+    this.tapListProviderDisplay,
     this.name,
-    {
-      this.timeZone,
-      this.address,
-      this.city,
-      this.state,
-      this.postalCode,
-      this.url,
-      this.facebookURL,
-      this.twitterHandle,
-      this.instagramHandle,
-      this.untappdURL,
-    }
-  );
+    this.address,
+    this.city,
+    this.state,
+    this.postalCode,
+    this.country,
+    this.website,
+    this.facebookPage,
+    this.twitterHandle,
+    this.instagramHandle,
+    this.tapListProvider,
+    this.untappdUrl,
+    this.email,
+    this.phoneNumber,
+    this.logoUrl,
+    this.slug});
 
-  Venue.fromJson(Map<String, dynamic> json):
-    name = json['name'],
-    untappdURL = json['untappd_url'],
-    address = json['address'],
-    id = json['id'],
-    city = json['city'],
-    state = json['state'],
-    postalCode = json['postal_code'],
-    url = json['website'],
-    facebookURL = json['facebook_url'],
-    twitterHandle = json['twitter_handle'],
-    timeZone = json['time_zone'],
+  Venue.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    timeZone = json['time_zone'];
+    tapListProviderDisplay = json['tap_list_provider_display'];
+    name = json['name'];
+    address = json['address'];
+    city = json['city'];
+    state = json['state'];
+    postalCode = json['postal_code'];
+    country = json['country'];
+    website = json['website'];
+    facebookPage = json['facebook_page'];
+    twitterHandle = json['twitter_handle'];
     instagramHandle = json['instagram_handle'];
+    tapListProvider = json['tap_list_provider'];
+    untappdUrl = json['untappd_url'];
+    email = json['email'];
+    phoneNumber = json['phone_number'];
+    logoUrl = json['logo_url'];
+    slug = json['slug'];
+  }
 }
