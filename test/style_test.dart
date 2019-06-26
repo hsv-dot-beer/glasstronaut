8fa3cf3a0796eb111b0db7925918cad93c736b55
Drew Winstel <drewbrew@users.noreply.github.com>
2019-06-25 20:56:31 -0500

    Add style model

---
+++ test/style_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:beers/style.dart';
+
+void main() {
+  test('Style from JSON is valid', () {
+    final json = {
+      "id": 537,
+      "alternate_names": [
+        "style1", "style2", "style3",
+      ],
+      "name": "Bock - Hell / Maibock / Lentebock"
+    };
+    final style = Style.fromJson(json);
+    expect(style.id, 537);
+    expect(style.name, "Bock - Hell / Maibock / Lentebock");
+    expect(style.alternateNames.length, 3);
+    expect(style.alternateNames, ["style1", "style2", "style3"]);
+  });
+}
