2d32da7092534421d7332365855e55e54771c6fb
Drew Winstel <drewbrew@users.noreply.github.com>
2019-07-13 15:04:41 -0500

    Add the first couple of cards
    - Beer overview
    - Manufacturer overview
    - Initial untappd metadata

--- lib/beer_detail.dart
+++ lib/beer_detail.dart
@@ -1,23 +1,93 @@
-import 'dart:async';
-import 'dart:convert';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_pagewise/flutter_pagewise.dart';
-import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+import 'package:decimal/decimal.dart';
 
 import 'beer.dart';
 import 'beer_icons.dart';
-import 'venue.dart';
-
 
 class BeerDetailView extends StatelessWidget {
-
   final Beer beer;
-  BeerDetailView({this.beer}): super();
+
+  BeerDetailView({this.beer}) : super();
 
   @override
   Widget build(BuildContext context) {
+    String subtitle = '${this.beer.style.name}';
+    if (this.beer.abv != Decimal.fromInt(0) && this.beer.ibu != null) {
+      subtitle += '\n${this.beer.abv}% ABV';
+    }
+    if (this.beer.ibu != 0 && this.beer.ibu != null) {
+      subtitle += '\n${this.beer.ibu} IBU';
+    }
+
+    Card beerCard = Card(
+      child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+        ListTile(
+          leading: (this.beer.logoUrl != null && this.beer.logoUrl != '')
+              ? Image.network(
+                  this.beer.logoUrl,
+                  height: 50,
+                  width: 50,
+                )
+              : null,
+          trailing: Icon(
+            BeerIcons.beer,
+            color: this.beer.color,
+          ),
+          title: Text('${this.beer.name}'),
+          subtitle: Text(subtitle),
+        )
+      ]),
+    );
+
+    Card breweryCard = Card(
+        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+      ListTile(
+        leading: (this.beer.manufacturer.logoUrl != null &&
+                this.beer.manufacturer.logoUrl != '')
+            ? Image.network(
+                this.beer.manufacturer.logoUrl,
+                height: 50,
+                width: 50,
+              )
+            : null,
+        trailing: (this.beer.manufacturer.url != null &&
+                this.beer.manufacturer.url != '')
+            ? GestureDetector(
+                child: Icon(
+                  IconData(59403, fontFamily: 'MaterialIcons'),
+                  color: Colors.lightBlue,
+                ),
+                onTap: () {
+                  _launchURL(this.beer.manufacturer.url);
+                },
+              )
+            : null,
+        title: Text(this.beer.manufacturer.name),
+        subtitle: Text(this.beer.manufacturer.location),
+      )
+    ]));
+
+    List<Widget> children = [beerCard, breweryCard];
+
+    if (this.beer.untappdMetadata != null) {
+      Card untappdCard = Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              title: Text('Untappd Information'),
+              subtitle: Text(
+                'Average rating: ${this.beer.untappdMetadata.jsonData.weightedRatingScore.toStringAsFixed((2))} (${this.beer.untappdMetadata.jsonData.ratingCount} ratings)'
+              ),
+            )
+          ]
+        )
+      );
+
+      children.add(untappdCard);
+    }
+
     return MaterialApp(
       title: this.beer.name,
       theme: new ThemeData(
@@ -29,8 +99,16 @@ class BeerDetailView extends StatelessWidget {
           appBar: AppBar(
             title: Text(this.beer.name),
           ),
-          body: Text('${this.beer.name} from ${this.beer.manufacturer.name}'),
+          body: Column(children: children),
       ),
     );
+  }
+}
+
+_launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
